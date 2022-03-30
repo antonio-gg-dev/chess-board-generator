7aaab385cfe3abbd37d21f51d120d55a2ff17a91
Katarn <katarn@katarn.es>
2022-03-30 22:48:25 +0200

    Add more customizations

--- app.js
+++ app.js
@@ -2,11 +2,40 @@ function updateBoard() {
   const body = document.querySelector('body');
   const oddColor = document.querySelector('#odd-color');
   const evenColor = document.querySelector('#even-color');
-  const separation = document.querySelector('#separation-amount');
+  const borderAmount = document.querySelector('#border-amount');
+  const borderColor = document.querySelector('#border-color');
+  const separationAmount = document.querySelector('#separation-amount');
+  const separationColor = document.querySelector('#separation-color');
+  const outerSeparation = document.querySelector('#outer-separation');
 
-  body.style.setProperty('--odd-color', oddColor.value);
-  body.style.setProperty('--even-color', evenColor.value);
-  body.style.setProperty('--separation-amount', separation.value + 'mm');
+  body.style.setProperty(
+    '--odd-color',
+    oddColor.value
+  );
+  body.style.setProperty(
+    '--even-color',
+    evenColor.value
+  );
+  body.style.setProperty(
+    '--border-amount',
+    borderAmount.value + 'mm'
+  );
+  body.style.setProperty(
+    '--border-color',
+    borderColor.value
+  );
+  body.style.setProperty(
+    '--separation-amount',
+    separationAmount.value + 'mm'
+  );
+  body.style.setProperty(
+    '--separation-color',
+    separationColor.value
+  );
+  body.style.setProperty(
+    '--outer-separation',
+    outerSeparation.checked ? 1 : 0
+  );
 }
 
 function resetBoard() {
